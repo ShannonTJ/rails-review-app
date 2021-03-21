c8e2162554dc8828dc036170721130996f0d1373
Shannon TJ <shannontj@shaw.ca>
2021-03-20 22:38:58 -0600

    Header component setup

---
+++ app/javascript/components/Airline/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div className="wrapper">
+      <h1>
+        <img src="" />
+        Airline Name
+      </h1>
+      <div className="total-reviews"></div>
+      <div className="rating"></div>
+      <div className="total-out-of"></div>
+    </div>
+  );
+};
+
+export default Header;
