a4cdf93b1e125d05c65a43b072ed38c149d1201f
Shannon TJ <shannontj@shaw.ca>
2021-03-20 21:43:40 -0600

    install axios to get json data

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Airlines = () => {
-  return <div>airlines component</div>;
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    //get all airlines from api
+    //update airlines in our state
+    axios
+      .get("/api/v1/airlines.json")
+      .then((resp) => console.log(resp))
+      .catch((resp) => console.log(resp));
+  }, [airlines.length]);
+
+  return <h1>Airlines</h1>;
 };
 
 export default Airlines;
