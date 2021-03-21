9c797b61f2ad33ebb8ea36adab91473e19a91d5c
Shannon TJ <shannontj@shaw.ca>
2021-03-20 22:29:08 -0600

    separate styled components

--- app/javascript/components/Airlines/AirlinesStyle.js
+++ app/javascript/components/Airlines/AirlinesStyle.js
@@ -1,13 +1,13 @@
 import styled from "styled-components/macro";
 
-const Home = styled.div`
+export const Home = styled.div`
   text-align: center;
   max-width: 1200px;
   margin-left: auto;
   margin-right: auto;
 `;
 
-const Header = styled.div`
+export const Header = styled.div`
   padding: 100px 100px 10px 100px;
 
   h1 {
@@ -15,12 +15,12 @@ const Header = styled.div`
   }
 `;
 
-const Subheader = styled.div`
+export const Subheader = styled.div`
   font-weight: 300;
   font-size: 26px;
 `;
 
-const Grid = styled.div`
+export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 20px;
