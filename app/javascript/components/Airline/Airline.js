34edd6f9f242e6a626a5578575360458abce6ea6
Shannon TJ <shannontj@shaw.ca>
2021-03-20 21:37:44 -0600

    add react router dom

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Airline = () => {
+  return <div>airline component</div>;
+};
+
+export default Airline;
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Airlines = () => {
+  return <div>airlines component</div>;
+};
+
+export default Airlines;
