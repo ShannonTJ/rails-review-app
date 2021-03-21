0381e7048e657f79f33ef5097f2eccdf184d0be2
Shannon TJ <shannontj@shaw.ca>
2021-03-20 22:26:36 -0600

    separate styled components

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,35 +1,7 @@
 import React, { useState, useEffect } from "react";
 import GridItem from "./GridItem";
 import axios from "axios";
-import styled from "styled-components";
-
-const Home = styled.div`
-  text-align: center;
-  max-width: 1200px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Header = styled.div`
-  padding: 100px 100px 10px 100px;
-
-  h1 {
-    font-size: 42px;
-  }
-`;
-
-const Subheader = styled.div`
-  font-weight: 300;
-  font-size: 26px;
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 20px;
-  width: 100%;
-  padding: 20px;
-`;
+import { Home, Header, Subheader, Grid } from "./AirlinesStyle";
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
@@ -41,17 +13,17 @@ const Airlines = () => {
       .catch((resp) => console.log(resp));
   }, [airlines.length]);
 
-  const grid = airlines.map((item) => {
-    return <GridItem key={item.attributes.name} attributes={item.attributes} />;
-  });
-
   return (
     <Home>
       <Header>
         <h1>RubyReviews</h1>
         <Subheader>A React/Rails project.</Subheader>
       </Header>
-      <Grid>{grid}</Grid>
+      <Grid>
+        {airlines.map((item) => (
+          <GridItem key={item.attributes.name} attributes={item.attributes} />
+        ))}
+      </Grid>
     </Home>
   );
 };
