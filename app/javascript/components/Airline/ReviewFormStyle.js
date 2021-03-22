8c2f67d36598ab7eb672416086ee94b1445415ab
Shannon TJ <shannontj@shaw.ca>
2021-03-21 18:55:35 -0600

    separate the review form styling into its own file

---
+++ app/javascript/components/Airline/ReviewFormStyle.js
@@ -0,0 +1,108 @@
+import styled from "styled-components/macro";
+import Grey from "./Stars/Grey";
+import Gold from "./Stars/Gold";
+import Hover from "./Stars/Hover";
+
+export const RatingContainer = styled.div`
+  text-align: center;
+  border-radius: 4px;
+  font-size: 18px;
+  padding: 40px 0 10px 0;
+  border: 1px solid #e6e6e6;
+  background: #fff;
+`;
+
+export const RatingBox = styled.div`
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  flex-direction: row-reverse;
+  position: relative;
+  margin-top: 12px;
+
+  input {
+    display: none;
+  }
+
+  label {
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    background-image: url("data:image/svg+xml;charset=UTF-8,${Grey}");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 70%;
+  }
+
+  input:checked ~ label,
+  input:checked ~ label ~ label {
+    background-image: url("data:image/svg+xml;charset=UTF-8,${Gold}");
+  }
+
+  input:not(:checked) ~ label:hover,
+  input:not(:checked) ~ label:hover ~ label {
+    background-image: url("data:image/svg+xml;charset=UTF-8,${Hover}");
+  }
+`;
+
+export const Field = styled.div`
+  border-radius: 4px;
+
+  input {
+    min-height: 50px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 0 0 12px 0;
+    padding: 12px;
+    width: 96%;
+  }
+
+  textarea {
+    width: 100%;
+    min-height: 80px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 12px 0;
+    padding: 12px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  background: #fff;
+  padding: 20px;
+  background: #000;
+  height: 100vh;
+  padding-top: 100px;
+`;
+
+export const Submit = styled.button`
+  color: #fff;
+  background: #333;
+  border-radius: 4px;
+  padding: 12px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: ease-in-out 0.1s;
+  border: 1px solid #fff;
+  width: 100%;
+  margin-top: 20px;
+
+  &:hover {
+    background: #fff;
+    color: #000;
+    border: 1px solid #fff;
+  }
+`;
+
+export const Headline = styled.div`
+  padding: 20px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+export const RatingTitle = styled.div`
+  font-size: 20px;
+  padding-bottom: 20px;
+  font-weight: bold;
+`;
