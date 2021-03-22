67b3864c48c5cda858b3889bf284ee0fb099b30e
Shannon TJ <shannontj@shaw.ca>
2021-03-21 18:51:01 -0600

    styling the review form page

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -73,6 +73,11 @@ const Airline = (props) => {
       .catch((resp) => {});
   };
 
+  const setRating = (score, e) => {
+    e.preventDefault();
+    setReview({ ...review, score });
+  };
+
   return (
     <Wrapper>
       {loaded && (
@@ -91,6 +96,7 @@ const Airline = (props) => {
             <ReviewForm
               handleChange={handleChange}
               handleSubmit={handleSubmit}
+              setRating={setRating}
               attributes={airline.data.attributes}
               review={review}
             />
